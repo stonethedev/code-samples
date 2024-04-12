f68f009c95db7b451871de509304ea5a09e117a2
Chase Stone <chase@stonethe.dev>
2024-04-12 13:22:33 -0400

    Create python-lambda-functions.py

---
+++ python-lambda-functions.py
@@ -0,0 +1,15 @@
+# Example 1:
+double = lambda x: x * 2
+print(double(5))
+
+# Example 2:
+add = lambda a, b: a + b
+print(add(3, 5))
+
+# Example 3:
+multiply = lambda x, y, z: x * y * z
+print(multiply(2, 3, 4))
+
+# Example 4:
+is_even = lambda num: num % 2 == 0
+print(is_even(6))

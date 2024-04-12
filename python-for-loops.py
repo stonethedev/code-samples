828216a8636e74986e464ec65745c4ce39763083
Chase Stone <chase@stonethe.dev>
2024-04-12 13:15:58 -0400

    Create python-for-loops.py

---
+++ python-for-loops.py
@@ -0,0 +1,19 @@
+# Example 1:
+for i in range(5):
+    print(i)
+
+# Example 2:
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# Example 3:
+for char in "Hello":
+    print(char)
+
+# Example 4:
+numbers = [1, 2, 3, 4, 5]
+sum = 0
+for num in numbers:
+    sum += num
+print("The sum is:", sum)
